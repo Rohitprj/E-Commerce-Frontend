f6d665fef0483610e9fc23142e23480771dfb6c5
Rohit kumar <rohitprj.1282@gmail.com>
2025-04-01 19:06:46 +0530

    refactor header component to enhance layout and add navigation buttons

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,13 +3,39 @@ import React from "react";
 export default function header() {
   return (
     <div>
-      <div className="w-full border-b-1 flex">
+      <div className="w-full border-b-1 flex justify-between">
         <p className="text-black font-bold text-[38px] px-8 py-3">E-Commerce</p>
-        <p className="text-white text-[22px] bg-black h-12">Home</p>
-        <p className="text-[22px] h-12">About</p>
-        <p className="text-[22px] h-12">Feature</p>
-        <p className="text-[22px] h-12">Pricing</p>
-        <p className="text-[22px] h-12">Taxes</p>
+        <div className="flex self-center gap-8 py-4 bg-amber-700">
+          <button className="text-white text-[20px] bg-black px-5 py-2 rounded-full transition-all duration-700 ease-in-out hover:bg-gray-800">
+            Home
+          </button>
+
+          <p className="text-[20px] flex items-center px-4 border border-transparent hover:border-black rounded-full transition duration-300 cursor-pointer">
+            About
+          </p>
+
+          <p className="text-[20px] flex items-center px-4 border border-transparent hover:border-black rounded-full transition duration-300 cursor-pointer">
+            Feature
+          </p>
+          <p className="text-[20px] flex items-center px-4 border border-transparent hover:border-black rounded-full transition duration-300 cursor-pointer">
+            Pricing
+          </p>
+          <p className="text-[20px] flex items-center px-4 border border-transparent hover:border-black rounded-full transition duration-300 cursor-pointer">
+            Taxes
+          </p>
+        </div>
+        {/* <div className="flex self-center">
+          <button className="text-[20px] flex items-center">Sign up</button>
+          <button className="text-[20px] h-12 flex items-center">Log in</button>
+        </div> */}
+        <div className="flex self-center">
+          <button className="text-[20px] flex items-center px-10 py-6 border-l border-black">
+            Sign up
+          </button>
+          <button className="text-[20px] flex items-center px-10 py-6 border-l border-black">
+            Log in
+          </button>
+        </div>
       </div>
     </div>
   );
