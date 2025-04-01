c6e6f50a142f65f275b26fdcf4e1b8c2c3302fcb
Rohit kumar <rohitprj.1282@gmail.com>
2025-04-01 18:06:52 +0530

    add header component and update Home page to use it

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,14 @@ import React from "react";
 export default function header() {
   return (
     <div>
-      <p>header</p>
+      <div className="w-full border-b-1 flex">
+        <p className="text-black font-bold text-[38px] px-8 py-3">E-Commerce</p>
+        <p className="text-white text-[22px] bg-black h-12">Home</p>
+        <p className="text-[22px] h-12">About</p>
+        <p className="text-[22px] h-12">Feature</p>
+        <p className="text-[22px] h-12">Pricing</p>
+        <p className="text-[22px] h-12">Taxes</p>
+      </div>
     </div>
   );
 }
