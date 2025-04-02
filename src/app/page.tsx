6b745ea8f9ef61e3433d6b8ce93ef06c74928bfa
Rohit kumar <rohitprj.1282@gmail.com>
2025-04-02 18:49:06 +0530

    add HeroSection component and integrate it into the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/header";
+import HeroSection from "@/components/heroSection";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <HeroSection />
     </div>
   );
 }
---
+++ src/components/heroSection.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function heroSection() {
+  return (
+    <div>
+      <div>
+        <textarea name="search" id="search" className="bg-red-500"></textarea>
+      </div>
+    </div>
+  );
+}
