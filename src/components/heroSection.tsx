0f860d07e2c3af1e394f242273368232c310509e
Rohit kumar <rohitprj.1282@gmail.com>
2025-04-03 16:30:21 +0530

    enhance heroSection with close button and update styling

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { IoIosSearch } from "react-icons/io";
+import { IoIosClose, IoIosSearch } from "react-icons/io";
 
 export default function heroSection() {
   return (
     <div>
-      <div className="flex justify-center border-1 w-6xl py-3 px-6">
+      <div className="flex border-1 w-6xl py-3 px-2 m-10 rounded-sm">
         <button className="">
           <IoIosSearch size={22} />
         </button>
@@ -12,8 +12,12 @@ export default function heroSection() {
           type="text"
           name="search"
           id="search"
-          className="bg-green-300 h-7 w-6xl border-0 outline-none"
+          placeholder="Search Products"
+          className="bg-grn-300 h-7 w-full px-2 border-0 outline-none"
         />
+        <button className="">
+          <IoIosClose size={28} />
+        </button>
       </div>
     </div>
   );
