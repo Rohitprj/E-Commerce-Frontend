f6d3f27ccbd83d733859077b0a479f133996003d
Rohit kumar <rohitprj.1282@gmail.com>
2025-04-03 16:08:40 +0530

    add react-icons dependency and enhance heroSection with search button

--- src/components/heroSection.tsx
+++ src/components/heroSection.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { IoIosSearch } from "react-icons/io";
 
 export default function heroSection() {
   return (
     <div>
-      <div className="">
+      <div className="flex justify-center border-1 w-6xl py-3 px-6">
+        <button className="">
+          <IoIosSearch size={22} />
+        </button>
         <input
           type="text"
           name="search"
           id="search"
-          className="bg-green-300 h-16 w-6xl"
+          className="bg-green-300 h-7 w-6xl border-0 outline-none"
         />
       </div>
     </div>
