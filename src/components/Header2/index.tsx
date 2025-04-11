b6bbb68ad1e9c2026a9b693e62aaf96c53474963
Rohit kumar <rohitprj.1282@gmail.com>
2025-04-11 13:41:10 +0530

    fix: update button padding and hover effects for improved UI consistency in Header2 component

--- src/components/Header2/index.tsx
+++ src/components/Header2/index.tsx
@@ -35,9 +35,9 @@ export default function Header2() {
         <div className=" w-6xl pt-5">
           <button
             className="cursor-pointer items-center rounded-full 
-            border border-black bg-white px-4 py-2 text-lg transition-all shadow-none 
+            border border-black bg-white px-3 py-1 text-lg transition-all shadow-none 
             hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] 
-            hover:-translate-y-2 
+            hover:-translate-y-2 hover:-translate-x-2 
             active:translate-x-[3px] active:translate-y-[3px] active:shadow-none"
           >
             All
@@ -45,8 +45,8 @@ export default function Header2() {
 
           <button
             className="ml-5 cursor-pointer text-lg transition-all duration-200
-            hover:px-4 hover:py-2 hover:rounded-full hover:border hover:border-black hover:bg-white
-            hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] hover:-translate-y-1
+            hover:px-3 hover:py-1 hover:rounded-full hover:border hover:border-black hover:bg-white
+            hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] hover:-translate-y-1 
             active:translate-x-[3px] active:translate-y-[3px] active:shadow-none"
           >
             Clothes
@@ -54,7 +54,7 @@ export default function Header2() {
 
           <button
             className="ml-5 cursor-pointer text-lg transition-all duration-200
-            hover:px-4 hover:py-2 hover:rounded-full hover:border hover:border-black hover:bg-white
+            hover:px-3 hover:py-1 hover:rounded-full hover:border hover:border-black hover:bg-white
             hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] hover:-translate-y-1
             active:translate-x-[3px] active:translate-y-[3px] active:shadow-none"
           >
@@ -62,7 +62,7 @@ export default function Header2() {
           </button>
           <button
             className="ml-5 cursor-pointer text-lg transition-all duration-200
-            hover:px-4 hover:py-2 hover:rounded-full hover:border hover:border-black hover:bg-white
+            hover:px-3 hover:py-1 hover:rounded-full hover:border hover:border-black hover:bg-white
             hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] hover:-translate-y-1
             active:translate-x-[3px] active:translate-y-[3px] active:shadow-none"
           >
