d43a926c356ff813ec38a50d17ab15cab3eda1b9
Rohit kumar <rohitprj.1282@gmail.com>
2025-04-19 16:14:48 +0530

    feat: refine middleware logic for authentication redirects and update path matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,14 +3,20 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  console.log("HELLO");
-  console.log("Type", typeof request.cookies);
+  const path = request.nextUrl.pathname;
+  const publicPath = path === "/login" || path === "/signup";
+  const cookies = request.cookies.get("refreshToken")?.value || " ";
 
-  // return NextResponse.redirect(new URL("/", request.url));
+  if (publicPath || cookies) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+  if (!publicPath || !cookies) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
   return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/", "/about/:path*"],
+  matcher: ["/login", "/signup"],
 };
