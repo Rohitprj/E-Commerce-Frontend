6e9f9eba329c7cf90410191c97d69ea8a64bd941
Rohit kumar <rohitprj.1282@gmail.com>
2025-04-26 16:47:02 +0530

    fix: enhance middleware to handle dashboard route and improve cookie logging

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,26 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-// import { BASE_URL } from "../utils/axiosInstance";
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
   console.log("first");
   const path = request.nextUrl.pathname;
   const publicPath = path === "/login" || path === "/signup";
+  const dashboardPath = path === "/dashboard";
 
   const cookies = request.cookies.get("refreshToken")?.value;
-  // const cookies =
-  //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhleUBnbWFpbC5jb20iLCJfaWQiOiI2ODA2NDc3YjIyMDgxN2Y1MWJiN2I0OTYiLCJpYXQiOjE3NDUzMTkyNzYsImV4cCI6MTc0NTkyNDA3Nn0.98SBzGstzB_1zPT1DOsG7MZIdWzh6gW1AwpikrrAy3Y";
+  //  const cookies =
+  //  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImhleUBnbWFpbC5jb20iLCJfaWQiOiI2ODA2NDc3YjIyMDgxN2Y1MWJiN2I0OTYiLCJpYXQiOjE3NDUzMTkyNzYsImV4cCI6MTc0NTkyNDA3Nn0.98SBzGstzB_1zPT1DOsG7MZIdWzh6gW1AwpikrrAy3Y";
+  //  console.log("Cookie : ", cookies);
   console.log("Cookie : ", cookies);
 
   if (publicPath && cookies) {
+    // If logged in and trying to access login/signup, redirect to home
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  if (dashboardPath && !cookies) {
+    // If not authenticated and trying to access protected route (dashboard), redirect to home
     return NextResponse.redirect(new URL("/", request.url));
   }
 
@@ -22,5 +29,5 @@ export async function middleware(request: NextRequest) {
 
 // config
 export const config = {
-  matcher: ["/", "/login", "/signup"],
+  matcher: ["/", "/login", "/signup", "/dashboard"],
 };
