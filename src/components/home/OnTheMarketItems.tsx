1f2751ae41d58586f828904ee5caaaa72a4df60e
Rohit kumar <rohitprj.1282@gmail.com>
2025-04-30 12:21:14 +0530

    refactor: streamline key assignment in OnTheMarketItems component

--- src/components/home/OnTheMarketItems.tsx
+++ src/components/home/OnTheMarketItems.tsx
@@ -7,14 +7,12 @@ export default function OnTheMarketItems() {
       <section className="flex-1 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {products.map((items, index) => (
           <div
+            key={index}
             className="object-cover rounded-md cursor-pointer shadow-none 
                   hover:shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] 
                   active:translate-x-[3px] active:translate-y-[3px] active:shadow-none"
           >
-            <div
-              key={index}
-              className="w-64 h-120 border rounded-sm bg-white shadow-sm hover:shadow-md transition overflow-hidden"
-            >
+            <div className="w-64 h-120 border rounded-sm bg-white shadow-sm hover:shadow-md transition overflow-hidden">
               <div className="w-full h-60 border-b flex items-center justify-center bg-gray-100">
                 <img
                   src={items.image}
